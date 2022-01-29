3963e0a146361c3af160c162842eeda479dc43e0
Shimeat <dmitriykud-07@bk.ru>
2022-01-29 20:18:41 +0500

    0.0.0.1 (Create Routing file)

--- app.py
+++ app.py
@@ -3,14 +3,16 @@
 # Не редактировать FrontEnd-ерам, мало ли поломается все)!!
 
 from flask import Flask, render_template
+from route import route
 
-app = Flask(__name__, template_folder="templates")
+app = Flask(__name__)
+app.register_blueprint(route, url_prefix="")
 
 #Routing
 
-@app.route("/")
-def index():
-    return render_template("index.html")
+# @app.route("/")
+# def index():
+#     return render_template("index.html")
 
 if(__name__ == "__main__"):
     app.run(debug=True) 
---
+++ route.py
@@ -0,0 +1,10 @@
+from flask import Blueprint, render_template
+
+route = Blueprint("route". __name__, template_folder="templates")
+
+
+# Base Routing
+
+@route.route('/')
+def index():
+    return render_template('index.html')
