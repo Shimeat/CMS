12feceb7bdfc0a5e9e8207b0b5a5a737d9ce2c34
Shimeat <dmitriykud-07@bk.ru>
2022-01-30 00:01:58 +0500

    add index.html

--- route.py
+++ route.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template
 
-route = Blueprint("route", __name__, template_folder="templates")
+route = Blueprint("route", __name__, template_folder="templates", static_folder="assets", static_url_path="assets")
 
 
 # Base Routing
