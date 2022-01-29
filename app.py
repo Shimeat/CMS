0bb299edfcae83194517f1db437cecfd628f1daa
Shimeat <dmitriykud-07@bk.ru>
2022-01-29 14:14:18 +0500

    start

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
 # CMS Project by Dmitrienko, Golovina, ..
 # <)
 # Не редактировать FrontEnd-ерам, мало ли поломается все)!!
+
+from flask import Flask
+
+app = Flask(__name__)
+
+#Routing
+
+@app.route("/")
+def index():
+    return "<h1>Ебать у вас тут CMS</h1>"
+
+if(__name__ == "__main__"):
+    app.run(debug=True)
+
